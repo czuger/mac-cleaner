efac0ed7ccffe3f3ce6e7f37cb527c17fb3b9f6e
ced <zuger.cedric@gmail.com>
2021-05-23 14:28:54 +0200

    address_keep.json added to protect some addresses.

--- clean_cookies.rb
+++ clean_cookies.rb
@@ -1,16 +1,16 @@
 require 'sqlite3'
-
-KEEP_COOKIES = [
-]
+require 'json'
 
 def keep_cookie?(row)
-  KEEP_COOKIES.each do |kc|
-    return true if kc[0] == row[0] && kc[1] == row[1]
+  $addr_keep.each do |addr|
+    if row[0].include?(addr)
+      return true
+    end
   end
-  false
+  return false
 end
 
-def delete_cookie?(db, row)
+def delete_cookie(db, row)
   db.execute( "DELETE FROM cookies WHERE host_key = '#{row[0]}' AND name = '#{row[1]}'" )
 end
 
@@ -20,10 +20,17 @@ def clean_cookies(root_dir, path)
   db = SQLite3::Database.new 'Cookies'
 
   db.execute( 'select host_key, name, path from cookies' ) do |row|
-    puts "Deleting #{row[0]}, #{row[1]}"
-    delete_cookie?(db, row) unless keep_cookie?(row)
+    unless keep_cookie? row
+      puts "Deleting #{row[0]}, #{row[1]}"
+      delete_cookie(db, row)
+    else
+      puts "Keeping #{row[0]}, #{row[1]}"
+    end
   end
 end
+
+s = File.read('address_keep.json')
+$addr_keep = JSON.parse(s)
 
 root_dir = '/' + Dir.pwd.split('/')[1..2].join('/')
 
